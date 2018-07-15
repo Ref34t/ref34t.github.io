9a89bff4390fa9bfb6849f3ef49034ca909772e0
Mohamed Refaat <mohammedref34t@gmail.com>
2018-07-15 14:57:54 +0200

    implement new layout

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@ var mainSection = document.querySelector(".main");
 var aboutSection = document.querySelector(".about");
 var portfolioSection = document.querySelector(".portfolio");
 var contactSection = document.querySelector(".contact");
+var icon = document.querySelectorAll(".icon");
+var span = document.querySelectorAll(".iconspan");
 
-TweenMax.from(mainSection, 1, {
-  opacity: 0,
-  x: -500
-});
-
+for (var i = 0; i < icon.length; i++) {
+  icon[i].addEventListener("mouseenter", function() {
+    TweenMax.to(this, 0.3, {
+      x: 50,
+      scale: 1.3
+    });
+    TweenMax.to(this.children, 0.3, {
+      opacity: 0
+    });
+  });
+  icon[i].addEventListener("mouseleave", function() {
+    TweenMax.to(this, 0.3, {
+      x: 0,
+      scale: 1
+    });
+    TweenMax.to(this.children, 0.3, {
+      opacity: 1
+    });
+  });
+}
 /*button1.addEventListener("click", function() {
   var tl = new TimelineMax();
   tl.to(section2, 0.2, {
