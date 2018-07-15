0297477eb36550638d90606715725923f0d2cc47
Mohamed Refaat <mohammedref34t@gmail.com>
2018-07-15 19:44:40 +0200

    working button

--- js/script.js
+++ js/script.js
@@ -1,143 +1,103 @@
-var mainSection = document.querySelector(".main");
-var aboutSection = document.querySelector(".about");
-var portfolioSection = document.querySelector(".portfolio");
-var contactSection = document.querySelector(".contact");
-var icon = document.querySelectorAll(".icon");
-var span = document.querySelectorAll(".iconspan");
+// Variables Declarations
+const mainSection = document.querySelector(".main");
+const aboutSection = document.querySelector(".about");
+const portfolioSection = document.querySelector(".portfolio");
+const contactSection = document.querySelector(".contact");
+const icon = document.querySelectorAll(".icon");
+const span = document.querySelectorAll(".iconspan");
+const aboutButton = document.querySelector(".aboutbutton");
+const portfolioButton = document.querySelector(".portfoliobutton");
+const contactButton = document.querySelector(".contactbutton");
 
+// Grapping the enter buttons in array and loop through it to get the right button and apply the mouseenter and mouseleave event
 for (var i = 0; i < icon.length; i++) {
   icon[i].addEventListener("mouseenter", function() {
-    TweenMax.to(this, 0.3, {
-      x: 50,
-      scale: 1.3
+    TweenMax.to(this, 0.2, {
+      x: 10,
+      scale: 1.5,
+      rotation: 90,
+      y: 80
     });
-    TweenMax.to(this.children, 0.3, {
+    TweenMax.to(this.children, 0.2, {
       opacity: 0
     });
   });
   icon[i].addEventListener("mouseleave", function() {
-    TweenMax.to(this, 0.3, {
+    TweenMax.to(this, 0.2, {
       x: 0,
-      scale: 1
+      scale: 1,
+      y: 0,
+      rotation: 0
     });
-    TweenMax.to(this.children, 0.3, {
+    TweenMax.to(this.children, 0.2, {
       opacity: 1
     });
   });
 }
-/*button1.addEventListener("click", function() {
-  var tl = new TimelineMax();
-  tl.to(section2, 0.2, {
-    opacity: 0,
-    x: -1000
-  });
-  tl.to(section3, 0.2, {
-    opacity: 0,
-    x: -1000
-  });
-  tl.to(section4, 0.2, {
-    opacity: 1,
-    x: -1000
-  });
 
-  tl.to(section1, 0.3, {
+// Open section by clicking on the button
+aboutButton.addEventListener("click", function() {
+  var tl = new TimelineMax();
+  tl.to(mainSection, 0.2, {
+    height: 0,
+    x: -1600
+  });
+  tl.to(portfolioSection, 0.2, {
+    opacity: 0,
+    x: -1600
+  });
+  tl.to(contactSection, 0.2, {
+    opacity: 0,
+    x: -1600
+  });
+  tl.to(aboutSection, 0.1, {
     height: window.innerHeight
   });
-  tl.to("button", 0.2, {
-    x: 500
-  });
-  tl.to(backbutton, 0.2, {
-    opacity: 1,
-    x: 500
-  });
+  if ((aboutButton.innerText = "Enter")) {
+    aboutButton.innerText = "Back";
+  } else {
+    aboutButton.innerText = "Enter";
+  }
 });
-section2.addEventListener("click", function() {
+
+portfolioButton.addEventListener("click", function() {
   var tl = new TimelineMax();
-  tl.to(section1, 0.2, {
+  tl.to(mainSection, 0.2, {
+    height: 0,
+    x: -1600
+  });
+  tl.to(aboutSection, 0.2, {
     opacity: 0,
     height: 0,
-    x: -500
+    x: -1600
   });
-  tl.to(section3, 0.2, {
+  tl.to(contactSection, 0.2, {
     opacity: 0,
-    x: -500
+    x: -1600
   });
-  tl.to(section4, 0.2, {
-    opacity: 0,
-    x: -500
-  });
-
-  tl.to(section2, 0.2, {
+  tl.to(portfolioSection, 0.1, {
     height: window.innerHeight
   });
 });
-section3.addEventListener("click", function() {
+
+contactButton.addEventListener("click", function() {
   var tl = new TimelineMax();
-  tl.to(section1, 0.2, {
+  tl.to(mainSection, 0.2, {
+    height: 0,
+    opacity: 0,
+    x: -1600
+  });
+  tl.to(aboutSection, 0.2, {
     opacity: 0,
     height: 0,
-    x: -500
+    x: -1600
   });
-  tl.to(section2, 0.2, {
-    opacity: 0,
-    x: -500,
-    height: 0
-  });
-  tl.to(section4, 0.2, {
-    opacity: 0,
-    x: -500
-  });
-
-  tl.to(section3, 0.2, {
-    height: window.innerHeight
-  });
-});
-section4.addEventListener("click", function() {
-  var tl = new TimelineMax();
-  tl.to(section1, 0.2, {
+  tl.to(portfolioSection, 0.2, {
     opacity: 0,
     height: 0,
-    x: -500
+    x: -1600
   });
-  tl.to(section2, 0.2, {
-    opacity: 0,
-    x: -500,
-    height: 0
-  });
-  tl.to(section3, 0.2, {
-    opacity: 0,
-    x: -500,
-    height: 0
-  });
-
-  tl.to(section4, 0.2, {
+  tl.to(contactSection, 0.1, {
     height: window.innerHeight
   });
 });
-backbutton.addEventListener("click", function() {
-  var tl = new TimelineMax();
-  tl.to(backbutton, 0.1, {
-    opacity: 0,
-    x: -500
-  });
-  tl.to(section1, 0.2, {
-    opacity: 1,
-    height: innerHeight / 4
-  });
-  tl.to(section2, 0.2, {
-    opacity: 1,
-    height: innerHeight / 4,
-    x: 0
-  });
-  tl.to(section3, 0.2, {
-    opacity: 1,
-    height: innerHeight / 4,
-    x: 0
-  });
-
-  tl.to(section4, 0.2, {
-    opacity: 1,
-    height: innerHeight / 4,
-    x: 0
-  });
-});*/
