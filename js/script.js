0348e2590a33879ef1714f7fe610e12d0001e5d5
Mohamed Refaat <mohammedref34t@gmail.com>
2018-07-16 23:19:01 +0200

    assign the animation time to a variable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Variables Declarations
+"use strict";
 const wrapper = document.querySelector(".wrapper");
 const mainSection = document.querySelector(".main");
 const aboutSection = document.querySelector(".about");
@@ -12,16 +13,16 @@ const contactButton = document.querySelector(".contactbutton");
 const sectioncontenta = document.querySelector(".section-content-a");
 const sectioncontentp = document.querySelector(".section-content-p");
 const sectioncontentc = document.querySelector(".section-content-c");
-
+const time = 0.3;
 // Grapping the enter buttons in array and loop through it to get the right button and apply the mouseenter and mouseleave event
 for (var i = 0; i < icon.length; i++) {
   icon[i].addEventListener("mouseenter", function() {
-    TweenMax.to(this, 0.1, {
+    TweenMax.to(this, time, {
       scale: 1.2
     });
   });
   icon[i].addEventListener("mouseleave", function() {
-    TweenMax.to(this, 0.1, {
+    TweenMax.to(this, time, {
       scale: 1
     });
   });
@@ -29,45 +30,45 @@ for (var i = 0; i < icon.length; i++) {
 
 function aboutEnter() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: 0,
     x: -1600
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     opacity: 0,
     x: -1600
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     opacity: 0,
     x: -1600
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     height: window.innerHeight
   });
   aboutButton.innerHTML = '<span class="iconspan">Back</span>';
   aboutButton.classList.add("back");
   aboutSection.classList.toggle("section-full");
-  tl.from(sectioncontenta, 0.3, { opacity: 1, x: -2000 });
+  tl.from(sectioncontenta, time, { opacity: 1, x: -2000 });
   sectioncontenta.style.display = "block";
 }
 function aboutBack() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: window.innerHeight / 4,
     x: 0,
     opacity: 1
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     height: window.innerHeight / 4
   });
   aboutButton.innerHTML = '<span class="iconspan">Enter</span>';
@@ -88,46 +89,46 @@ aboutButton.addEventListener("click", function() {
 
 function portfolioEnter() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: 0,
     x: -1600
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     opacity: 0,
     x: -1600,
     height: 0
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     opacity: 0,
     x: -1600
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     height: window.innerHeight
   });
   portfolioButton.innerHTML = '<span class="iconspan">Back</span>';
   portfolioButton.classList.add("back");
   portfolioSection.classList.toggle("section-full");
   sectioncontentp.style.display = "block";
-  tl.from(sectioncontentp, 0.3, { opacity: 0, x: -2000 });
+  tl.from(sectioncontentp, time, { opacity: 0, x: -2000 });
 }
 function portfolioBack() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: window.innerHeight / 4,
     x: 0,
     opacity: 1
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
@@ -149,47 +150,47 @@ portfolioButton.addEventListener("click", function() {
 
 function contactEnter() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: 0,
     x: -1600
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     opacity: 0,
     x: -1600,
     height: 0
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     opacity: 0,
     x: -1600,
     height: 0
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     height: window.innerHeight
   });
   contactButton.innerHTML = '<span class="iconspan">Back</span>';
   contactButton.classList.add("back");
   contactSection.classList.toggle("section-full");
   sectioncontentc.style.display = "block";
-  tl.from(sectioncontentc, 0.3, { opacity: 0, x: -2000 });
+  tl.from(sectioncontentc, time, { opacity: 0, x: -2000 });
 }
 function contactBack() {
   var tl = new TimelineMax();
-  tl.to(mainSection, 0.2, {
+  tl.to(mainSection, time, {
     height: window.innerHeight / 4,
     x: 0,
     opacity: 1
   });
-  tl.to(portfolioSection, 0.2, {
+  tl.to(portfolioSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(contactSection, 0.2, {
+  tl.to(contactSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
   });
-  tl.to(aboutSection, 0.2, {
+  tl.to(aboutSection, time, {
     height: window.innerHeight / 4,
     opacity: 1,
     x: 0
